54d38c200364714f05b5d00753a13550cfc1e33f
shs400 <tlagustn88@naver.com>
2019-03-29 14:08:54 +0900

    불필요코드 제거

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mimon
 미세먼지 지수를 알려줄 수 있는 웹사이트 개발
+
+```
+$ # Dev build
+$ npm install
+$ npm run dev
+```
+
+```
+$ npm install
+$ npm run build
+```
--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import reducer from './reducers/app';
 
 import App from './components/App';
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(reducer, applyMiddleware(ReduxThunk));
-
 
 ReactDOM.render(
   <Provider store={store}>
